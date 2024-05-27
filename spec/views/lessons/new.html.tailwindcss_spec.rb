30118642b3c2049d8c8f365a327594c034aeb053
Abdoul <abdoulramanediallo44@gmail.com>
2024-05-27 21:30:45 +0000

    adding lessons models

---
+++ spec/views/lessons/new.html.tailwindcss_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe "lessons/new", type: :view do
+  before(:each) do
+    assign(:lesson, Lesson.new(
+      title: "MyString",
+      description: "MyText",
+      paid: false,
+      course: nil
+    ))
+  end
+
+  it "renders new lesson form" do
+    render
+
+    assert_select "form[action=?][method=?]", lessons_path, "post" do
+
+      assert_select "input[name=?]", "lesson[title]"
+
+      assert_select "textarea[name=?]", "lesson[description]"
+
+      assert_select "input[name=?]", "lesson[paid]"
+
+      assert_select "input[name=?]", "lesson[course_id]"
+    end
+  end
+end

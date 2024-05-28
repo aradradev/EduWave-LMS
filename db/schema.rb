450a5da09c675783e758acb0aafd0b666ffb5d08
Abdoul <abdoulramanediallo44@gmail.com>
2024-05-28 23:40:56 +0000

    adding table in the schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_27_224221) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_28_232948) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -72,6 +72,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_27_224221) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "paid"
+    t.string "stripe_id"
+    t.text "premium"
   end
 
   create_table "lessons", force: :cascade do |t|
@@ -81,6 +84,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_27_224221) do
     t.bigint "course_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "position"
     t.index ["course_id"], name: "index_lessons_on_course_id"
   end
 
@@ -101,6 +105,10 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_27_224221) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

37314fca8a98bfe0547cdee3458879422ff3c489
Abdoul <abdoulramanediallo44@gmail.com>
2024-06-10 17:40:56 +0000

    adding admin course files scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,10 @@ Rails.application.routes.draw do
 
   post "/webhook" => "webhooks#stripe"
 
+  namespace :admin do
+    resources :courses
+  end
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check

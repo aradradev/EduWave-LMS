590ac502912723af57e6780e2d38c9bc93f3b825
Abdoul <abdoulramanediallo44@gmail.com>
2024-06-18 21:19:15 +0000

    adding course controller logic

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -6,12 +6,12 @@ class CoursesController < ApplicationController
     @courses = Course.all
     @user_unlocked_course = current_user&.course_users&.pluck(:course_id)
     @user_started_courses = current_user&.lesson_users&.joins(:lesson)&.pluck(:course_id)&.uniq
-    if @user_started_courses.present?
-      @user_course_progresses = @user_started_courses.map do |course_id|
-        course_lessons = Course.find(course_id).lessons.count
-        completed_lessons = current_user&.lesson_users&.joins(:lesson)&.where(completed: true, lesson: {course: course_id })&.count
-        { course_id: course_id, completed_percentage: (completed_lessons.to_f / course_lessons.to_f * 100).to_i }
-      end
+    return unless @user_started_courses.present?
+
+    @user_course_progresses = @user_started_courses.map do |course_id|
+      course_lessons = Course.find(course_id).lessons.count
+      completed_lessons = current_user&.lesson_users&.joins(:lesson)&.where(completed: true, lesson: { course: course_id })&.count
+      { course_id:, completed_percentage: (completed_lessons.to_f / course_lessons * 100).to_i }
     end
   end
 

e6e2faeca6835221dde03b473d7674f8242689e2
Abdoul <abdoulramanediallo44@gmail.com>
2024-06-18 21:19:26 +0000

    adding lessons controller logic

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -32,17 +32,15 @@ class LessonsController < ApplicationController
   def set_lesson
     @lesson = Lesson.find(params[:id])
   end
-  
-  def check_paid
-    if @lesson.paid && !current_user.course_users.where(course_id: params[:course_id]).exists?
-      if @lesson.previous_lesson
-        redirect_to course_lesson_path(@course, @lesson.previous_lesson), notice: "You must purchase the full course to access the next lesson."
 
-      else
-        redirect_to course_path(@course), notice: "You must purchase the full course to access the next lesson."
-      end
+  def check_paid
+    return unless @lesson.paid && !current_user.course_users.where(course_id: params[:course_id]).exists?
+
+    if @lesson.previous_lesson
+      redirect_to course_lesson_path(@course, @lesson.previous_lesson), notice: 'You must purchase the full course to access the next lesson.'
+
+    else
+      redirect_to course_path(@course), notice: 'You must purchase the full course to access the next lesson.'
     end
   end
-    
-
 end
